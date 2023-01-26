0d2b904b3e222976fdfe26ff411b0fd4c99b07ba
NicolasAguila <ignacio.aguila.parra@gmail.com>
2023-01-26 03:07:59 -0300

    add Auth

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,4 +1,5 @@
 import Veterinario from "../models/Veterinario.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
     const { email } = req.body;
@@ -10,7 +11,7 @@ const registrar = async (req, res) => {
         const error = new Error('Usuario ya registrado');
         return res.status(400).json({msg: error.message})
     }
-
+    
     try {
         //Guardar un nuevo veterinario
         const veterinario = new Veterinario(req.body);
@@ -20,8 +21,6 @@ const registrar = async (req, res) => {
     } catch (error) {
         console.log(error)
     }
-
-    
 };
 
 const perfil = (req, res) => {
@@ -46,10 +45,38 @@ const confirmar = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+const autenticar = async (req, res) => {
+    const { email, password } = req.body;
+
+    //Comprobar si el usuario existe
+    const usuario = await Veterinario.findOne({email});
+
+    if(!usuario){
+        const error = new Error("El Usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    //Comprobar si el usuario esta confirmado
+    if(!usuario.confirmado){
+        const error = new Error('Tu Cuenta no ha sido confirmada');
+        return res.status(403).json({ msg: error.message });
+    }
+
+    //Revisar el password
+    if( await usuario.comprobarPassword(password)) {
+        //Autenticar
+        res.json({ token: generarJWT(usuario.id) })
+    }else{
+        const error = new Error('El Password es incorrecto');
+        return res.status(403).json({ msg: error.message });
+    }
+};
 
 export {
     registrar,
     perfil,
-    confirmar
+    confirmar,
+    autenticar
 };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,9 @@
+
+const checkAuth = (req, res, next) => {
+
+    console.log('Desde mi middleware');
+
+    next();
+};
+
+export default checkAuth;
