95944239eb4a07c936d4391ecfaa044e9ea6e9a8
NicolasAguila <ignacio.aguila.parra@gmail.com>
2023-01-26 23:23:42 -0300

    modules, routing and controllers for Veterinarios finished

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,13 +1,26 @@
 import  express  from 'express';
 const router = express.Router();
-import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js';
+import { 
+    registrar, 
+    perfil, 
+    confirmar, 
+    autenticar, 
+    olvidePassword, 
+    comprobarToken, 
+    nuevoPassword 
+} from '../controllers/veterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
-
+// Area publica
 router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+// router.get('/olvide-password/:token', comprobarToken);
+// router.post('/olvide-password/:token', nuevoPassword);
 
+//Area privada
 router.get('/perfil', checkAuth, perfil);
 
 export default router;
