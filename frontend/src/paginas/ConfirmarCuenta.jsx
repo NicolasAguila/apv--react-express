71cecbbb3654faf4b20e20c371f1b9b537075c21
NicolasAguila <ignacio.aguila.parra@gmail.com>
2023-01-28 19:18:01 -0300

    add pages and modify AuthLayout

---
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -0,0 +1,10 @@
+
+const ConfirmarCuenta = () => {
+    return (
+        <>
+            <h1> Desde ConfirmaCuenta</h1>
+        </>
+    )
+}
+
+export default ConfirmarCuenta
---
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -0,0 +1,16 @@
+
+const OlvidePassword = () => {
+    return (
+        <>
+            <div>
+                1
+            </div>
+
+            <div>
+                2
+            </div>
+        </>
+  )
+}
+
+export default OlvidePassword
