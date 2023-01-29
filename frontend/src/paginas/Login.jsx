8673c5e47d6bcf4d5636a0403de1312e72f78620
NicolasAguila <ignacio.aguila.parra@gmail.com>
2023-01-29 20:56:09 -0300

    add advance Registrar, Login and OlvidePassword

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
         <>
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">
-                    Inicia Sesión y Administra tus 
-                    <span className="text-black"> Pacientes</span>
+                    Inicia Sesión y Administra tus {""}
+                    <span className="text-black">Pacientes</span>
                 </h1>
             </div>
 
-            <div>
+            <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
                 <form>
                     <div className="my-5">
                         <label className="uppercase text-gray-600 block text-xl font-bold">Email</label>
@@ -36,6 +37,16 @@ const Login = () => {
                         font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto " 
                     />
                 </form>
+
+                <nav className='mt-10 lg:flex lg:justify-between'>
+                    <Link 
+                        className='block text-center my-5 text-gray-500'
+                        to="/registrar">No tienes una cuenta? Registrate</Link>
+                    <Link
+                        className='block text-center my-5 text-gray-500'
+                        to="/olvide-password">Olvide mi Password</Link>
+                </nav>
+
             </div>
             
         </>
